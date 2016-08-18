39c1c310adf833e087d00a1b80f353532197b613
Maelle Vance <maellevance@gmail.com>
2016-08-17 21:47:27 -0700

    some progress on data. Still needs styling. A lot.

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.find('.byline span').text(this.author);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 365 / 1000) + ' years ago');
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
   $newArticle.find('h1').html(this.title);
   $newArticle.find('.article-body').html(this.body);
   $newArticle.attr('data-category', this.category);
--- js/data.js
+++ js/data.js
@@ -4,14 +4,14 @@ var myLocalData = [
   {
     title: 'Chanson D\'automne',
     category: 'Categorie',
-    publishedOn: '1866-08-01',
+    publishedOn: '01-01-2000',
     body: '<p>Les sanglots longs des violons de l\'automne blessent mon coeur d\'une langueur monotone. Tout suffocant, et bleme, quand sonne l\'heure je me souviens de jours anciens et je pleure. Et je m\'en vais au vent mauvais qui m\'emporte deca, dela, pareil a la feuille morte.</p>',
     author: 'Paul Verlaine'
   },
   {
     title: 'Et un sourir',
     category: 'Categorie',
-    publishedOn: '1951-06-06',
+    publishedOn: '01-01-2000',
     body:'<p>La nuit n\'est jamais complete. Il y a toujours, puisque je le dis. Puisque je l\'affirme. Au bout du chagrin. Une fenetre ouverte. Une fenetre eclairee. Il y a toujours un reve qui veille. Desir a combler, faim a satisfaire. Un coeur genereux. Une main tendue, une main ouverte. Des yeux attentifs. Une vie, la vie a se partager...</p>',
     author: 'Paul Eluard',
   },
@@ -19,7 +19,28 @@ var myLocalData = [
   {
     title: 'Je t\'adore a l\'egal',
     category: 'Categorie',
-    publishedOn: '1857-06-25',
+    publishedOn: '01-01-2000',
     body:'<p>Je t\'adore a l\'egal de la voute nocturne, O vase de tristesse, O grande taciturne, Et t\'aime d\'autant plus, belle, que tu me fuis, Et que tu me parais, ornement de mes nuits, Plus ironiquement accumuler les lieues Qui separent mes bras des immensites bleues. Je m\'avance a l’attaque, et je grimpe aux assauts, Comme apres un cadavre un choeur de vermisseaux, Et je cheris, o bete implacable et cruelle ! Jusqu\'a cette froideur par ou tu m\'es plus belle !</p>',
     author: 'Charles Baudelaire'
-  }];
+  },
+  {
+    title: 'Salmon Cookies',
+    category: '201',
+    publishedOn: '07-31-2016',
+    body: '<p>First "big" 201 project. I build a website for a simulation of a salmon cookies business. Project including a front facing html page with all the necessary info and a private sales projection page with a javascript generated projections table that gives the option of adding store, removing store and updating store data</p><img src="http://placehold.it/400x150">',
+    author: 'Maelle Vance'
+  },
+  {
+    title: 'BusMall',
+    category: '201',
+    publishedOn: '08-05-2016',
+    body: '<p>My second big 201 project. Similation of a market analysis of a Skymall-like product.</p><p>The app\'s purpose is to have the tester choose which product, of the three displayed images, they would be most likely to purchase, and then store, calculate, and visually display the resulting data.</p><p>To keep the product selection process as untainted as possible, you have been instructed to not allow any results to be shown to users until there have been a total of 25 selections made.</p><p>Keeping track of how many times each image is displayed and clicked, I generated charts to make the data available and readily usable for the team doing the market analysis.</p><img src="http://placehold.it/400x150">',
+    author: 'Maelle Vance'
+  },
+  {
+    title: 'Autism Screen',
+    category: '201',
+    publishedOn: '08-12-2016',
+    body: '<p>My final project for 301. "Autism Screen" allows parents to assess their child\'s risk for autism.</p><p>Using the M-CHAT, a modified checklist developed by researchers, we generate a score representing the assessed risk for the child and provide parents with additional resources for early intervention.</p><p>The parent can easily sign in to take the test, review the questions and, if needed, can later come back to log in and get their original score and all the resources.</p><img src="http://placehold.it/400x150">'
+  }
+];
