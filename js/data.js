d9e316e353cfd46061bbfbacc98d691a03cbfc29
Maelle Vance <maellevance@gmail.com>
2016-08-18 17:47:41 -0700

    MVP

--- js/data.js
+++ js/data.js
@@ -4,14 +4,14 @@ var myLocalData = [
   {
     title: 'Chanson D\'automne',
     category: 'Categorie',
-    publishedOn: '01-01-2000',
+    publishedOn: '',
     body: '<p>Les sanglots longs des violons de l\'automne blessent mon coeur d\'une langueur monotone. Tout suffocant, et bleme, quand sonne l\'heure je me souviens de jours anciens et je pleure. Et je m\'en vais au vent mauvais qui m\'emporte deca, dela, pareil a la feuille morte.</p>',
     author: 'Paul Verlaine'
   },
   {
     title: 'Et un sourir',
     category: 'Categorie',
-    publishedOn: '01-01-2000',
+    publishedOn: '',
     body:'<p>La nuit n\'est jamais complete. Il y a toujours, puisque je le dis. Puisque je l\'affirme. Au bout du chagrin. Une fenetre ouverte. Une fenetre eclairee. Il y a toujours un reve qui veille. Desir a combler, faim a satisfaire. Un coeur genereux. Une main tendue, une main ouverte. Des yeux attentifs. Une vie, la vie a se partager...</p>',
     author: 'Paul Eluard',
   },
@@ -19,7 +19,7 @@ var myLocalData = [
   {
     title: 'Je t\'adore a l\'egal',
     category: 'Categorie',
-    publishedOn: '01-01-2000',
+    publishedOn: '',
     body:'<p>Je t\'adore a l\'egal de la voute nocturne, O vase de tristesse, O grande taciturne, Et t\'aime d\'autant plus, belle, que tu me fuis, Et que tu me parais, ornement de mes nuits, Plus ironiquement accumuler les lieues Qui separent mes bras des immensites bleues. Je m\'avance a l’attaque, et je grimpe aux assauts, Comme apres un cadavre un choeur de vermisseaux, Et je cheris, o bete implacable et cruelle ! Jusqu\'a cette froideur par ou tu m\'es plus belle !</p>',
     author: 'Charles Baudelaire'
   },
