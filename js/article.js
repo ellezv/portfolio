cfd16572b47e81a79c2a83d0443bb6ad14f9279d
Maelle Vance <maellevance@gmail.com>
2016-08-23 19:18:02 -0700

    REFACTORED SHIT

--- js/article.js
+++ js/article.js
@@ -1,40 +1,59 @@
 'use strict';
-function Article(obj) {
-  for (keys in obj) {
-    this[keys] = obj[keys];
-  }
-}
+(function(module) {
+  function Article(obj) {
+    for ( var key in obj) {
+      this[key] = obj[key];
+    }
+  };
 
-Article.allArticles = [];
+  Article.allArticles = [];
 
-Article.prototype.toHtml = function() {
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  var source = $('#article-template').text();
-  var templateRender = Handlebars.compile(source);
+  Article.prototype.toHtml = function() {
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    var source = $('#article-template').text();
+    var templateRender = Handlebars.compile(source);
 
-  return templateRender(this);
-};
+    return templateRender(this);
+  };
 
-
-/* sort method so object in myLocalData array with most recent publishedOn(=property) date (=value) will be first in the array */
-Article.loadAll = function(inputData) {
-  inputData.sort(function(a, b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  }).forEach(function(ele) {
-    Article.allArticles.push(new Article(ele));
-  });
-};
-
-Article.fetchAll = function() {
-  if (localStorage.projects) {
-    Article.loadAll(JSON.parse(localStorage.projects));
-    articleView.renderIndex();
-  }else {
-    $.getJSON('data/projects.json', function(a) {
-      Article.loadAll(a);
-      localStorage.projects = JSON.stringify(a);
-      articleView.renderIndex();
+  Article.loadAll = function(inputData) {
+    Article.allArticles = inputData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    }).map(function(ele) {
+      return new Article(ele);
     });
-  }
-};
+  };
+
+  Article.fetchAll = function(nextFunction) {
+    if (localStorage.projects) {
+      $.ajax({
+        type: 'HEAD',
+        url: 'data/projects.json',
+        success: function(data, message, xhr) {
+          var eTag = xhr.getResponseHeader('eTag');
+          if (!localStorage.eTag || eTag !== localStorage.eTag) {
+            Article.getAll(nextFunction);
+          } else {
+            Article.loadAll(JSON.parse(localStorage.projects));
+            nextFunction();
+          }
+        }
+      });
+    } else {
+      Article.getAll(nextFunction);
+    }
+  };
+
+  Article.getAll = function(nextFunction) {
+    $.getJSON('data/projects.json', function(data, message, xhr) {
+      localStorage.eTag = xhr.getResponseHeader('eTag');
+      Article.loadAll(data);
+      localStorage.projects = JSON.stringify(data);
+
+      nextFunction();
+    });
+  };
+
+  module.Article = Article;
+})(window);
--- js/articleView.js
+++ js/articleView.js
@@ -1,42 +1,46 @@
 'use strict';
-var articleView = {};
+(function(module) {
+  var articleView = {};
 
-articleView.handleNavMenu = function() {
-  $('nav').on('click', '.nav-tab', function() {
-    $('.menu-content').hide();
-    var $id = $(this).attr('data-content');
-    $('main').find('#' + $id).fadeIn();
-  });
-  $('nav .nav-tab:first').click();
-};
+  articleView.handleNavMenu = function() {
+    $('nav').on('click', '.nav-tab', function() {
+      $('.menu-content').hide();
+      var $id = $(this).attr('data-content');
+      $('main').find('#' + $id).fadeIn();
+    });
+    $('nav .nav-tab:first').click();
+  };
 
-articleView.handleShowMore = function() {
-  $('.article-body').hide();
-  $('.screenshots').hide();
-  $('article').on('click', '.read-on', function() {
-    event.preventDefault();
-    console.log('THIS.PARENT', $(this).parent());
-    $(this).parent().find('.article-body').show();
-    $(this).parent().find('.screenshots').show();
-    $(this).html('Show less &rarr;').removeClass('read-on').addClass('show-less');
-  });
+  articleView.handleShowMore = function() {
+    $('.article-body').hide();
+    $('.screenshots').hide();
+    $('article').on('click', '.read-on', function() {
+      event.preventDefault();
+      console.log('THIS.PARENT', $(this).parent());
+      $(this).parent().find('.article-body').show();
+      $(this).parent().find('.screenshots').show();
+      $(this).html('Show less &rarr;').removeClass('read-on').addClass('show-less');
+    });
 
-  $('article').on('click', '.show-less', function() {
-    event.preventDefault();
-    $(this).parent().find('.article-body').hide();
-    $(this).parent().find('.screenshots').hide();
-    $(this).html('Read on &rarr;').removeClass('show-less').addClass('read-on');
-  });
-};
+    $('article').on('click', '.show-less', function() {
+      event.preventDefault();
+      $(this).parent().find('.article-body').hide();
+      $(this).parent().find('.screenshots').hide();
+      $(this).html('Read on &rarr;').removeClass('show-less').addClass('read-on');
+    });
+  };
 
 
-articleView.renderIndex = function() {
-  Article.allArticles.forEach(function(a) {
-    $('#articles').append(a.toHtml('#article-template'));
-  });
-  articleView.handleNavMenu();
-  articleView.handleShowMore();
-  $('.screenshots').flip();
-};
+  articleView.renderIndex = function() {
+    Article.allArticles.forEach(function(a) {
+      $('#articles').append(a.toHtml('#article-template'));
+    });
+    articleView.handleNavMenu();
+    articleView.handleShowMore();
+    $('.screenshots').flip();
+  };
 
-Article.fetchAll();
+  Article.fetchAll(articleView.renderIndex);
+  module.articleView = articleView;
+
+})(window);
